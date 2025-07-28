28f353e41ae3f687182f48b202d928c88194a179
sasaigoli05 <suhan.asaigoli@gmail.com>
2025-07-28 05:55:13 -0400

    updated environment.yml, transcription pipeline to handle transcription through CPU

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ if uploaded_file is not None:
     st.audio(uploaded_file, format="audio/mp3")
     if st.button("Transcribe"):
         with st.spinner("Transcribing..."):
-            device = "cuda:0" if torch.cuda.is_available() else "cpu"
-            transcribe = pipeline(task="automatic-speech-recognition",
-                                  model="vasista22/whisper-kannada-tiny",
-                                  chunk_length_s=30,
-                                  device=device)
+            device = "cpu"
+            transcribe = pipeline(
+                task="automatic-speech-recognition",
+                model="vasista22/whisper-kannada-tiny",
+                chunk_length_s=30,
+                device=0 if device == "cuda" else -1  # -1 tells HF to use CPU
+            )
             transcribe.model.config.forced_decoder_ids = transcribe.tokenizer.get_decoder_prompt_ids(language="kn", task="transcribe")
             
             # Save to temp file
--- transcribe.py
+++ transcribe.py
@@ -5,7 +5,13 @@ from transformers import pipeline
 audio = "/home/sasaigoli/Documents/DKProximityNetwork/trialgoat/Kannada-Female-Prathibha.mp3"
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-transcribe = pipeline(task="automatic-speech-recognition", model="vasista22/whisper-kannada-tiny", chunk_length_s=30, device=device)
+device = "cpu"
+transcribe = pipeline(
+    task="automatic-speech-recognition",
+    model="vasista22/whisper-kannada-tiny",
+    chunk_length_s=30,
+    device=0 if device == "cuda" else -1  # -1 tells HF to use CPU
+)
 transcribe.model.config.forced_decoder_ids = transcribe.tokenizer.get_decoder_prompt_ids(language="kn", task="transcribe")
 
 # print('Transcription: ', transcribe(audio)["text"])
